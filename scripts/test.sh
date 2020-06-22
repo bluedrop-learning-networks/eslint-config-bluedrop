d10fa726eb672ccb61c8925c62289acc77ec74f0
Tim Oram <dev@mitmaro.ca>
2020-06-22 13:54:27 -0230

    Improve tests to detect deprecated rules
    This updates the tests to find and report rules in the configurations
    that have been deprecated.

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,3 +13,9 @@ npm run eslint -- -c config/node.js config/node.js "$@"
 
 # react config will fail against its self, so test against any empty file
 printf "" | npm run eslint -- --stdin -c config/react.js "$@"
+
+npm run check-deprecated --deprecated config/babel.js
+npm run check-deprecated --deprecated config/chai.js
+npm run check-deprecated --deprecated config/console.js
+npm run check-deprecated --deprecated config/mocha.js
+npm run check-deprecated --deprecated config/react.js
